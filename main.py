01c99a3afdbcf4d717832a5b3dadb6ea7751a70d
Denise <deniselewis1120@gmail.com>
2025-02-03 18:34:36 -0800

    Add additional tables to database

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     # Create tables
     cursor.executescript("""
     CREATE TABLE IF NOT EXISTS parks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id TEXT PRIMARY KEY,
         name TEXT NOT NULL,
         url TEXT NOT NULL,
         park_code TEXT NOT NULL,
@@ -24,6 +24,49 @@ def main():
         designation TEXT NOT NULL,
         image_url TEXT NOT NULL,
         is_free BOOLEAN
+    );
+
+    CREATE TABLE IF NOT EXISTS activities (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS amenities (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL,
+        categories[] TEXT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS topics (
+        id TEXT PRIMARY KEY,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE parks_activities (
+        park_id TEXT NOT NULL,
+        activity_id TEXT NOT NULL,
+        park_code TEXT,
+        PRIMARY KEY (park_id, activity_id),
+        FOREIGN KEY (park_id) REFERENCES parks(id),
+        FOREIGN KEY (activity_id) REFERENCES activities(id)
+    );
+
+    CREATE TABLE parks_amenities (
+        park_id TEXT NOT NULL,
+        amenity_id TEXT NOT NULL,
+        park_code TEXT,
+        PRIMARY KEY (park_id, amenity_id),
+        FOREIGN KEY (park_id) REFERENCES parks(id),
+        FOREIGN KEY (amenity_id) REFERENCES amenities(id)
+    );
+
+    CREATE TABLE parks_topics (
+        park_id TEXT NOT NULL,
+        topic_id TEXT NOT NULL,
+        park_code TEXT,
+        PRIMARY KEY (park_id, topic_id),
+        FOREIGN KEY (park_id) REFERENCES parks(id),
+        FOREIGN KEY (topic_id) REFERENCES topics(id)
     );
     """)
 
